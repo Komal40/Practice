76772dce8592583f469a6b023d8a5250a4ce5965
Jyoti Pandey <jyotip9352@gmail.com>
2023-10-09 19:27:47 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <>
-    <Navbar/>
+    {/* <Navbar/> */}
+    <Nav/>
     <Home/>
     </>
   );
